f7dfe99052e9a324d0b6f13a46beda6cd69e4bef
Tetiana Sobko <sobkotiana@gmail.com>
2026-07-15 13:56:59 +0100

    Add password hashing and verification functions

--- src/security/__init__.py
+++ src/security/__init__.py
@@ -1,3 +1,4 @@
+from security.passwords import hash_password, verify_password
 from security.utils import generate_secure_token
 
-__all__ = ["generate_secure_token"]
+__all__ = ["generate_secure_token", "hash_password", "verify_password"]
